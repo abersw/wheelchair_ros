4e6bf92378bc3fd473f61be2f391da1620652860
Tomos Fearn <tfearn10@outlook.com>
2020-09-20 19:53:09 +0100

    added docs site

---
+++ docs/js/footer.js
@@ -0,0 +1,2 @@
+
+	document.getElementById('footer').innerHTML = "This web page is maintained by Tomos Fearn, <a href='mailto:tof7@aber.ac.uk'>tof7@aber.ac.uk</a>. This site is version 1.0 | Release.";
